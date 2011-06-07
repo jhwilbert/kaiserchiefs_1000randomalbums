9f05a59c1a2c0b57e5411f44302642078f2a4332
joao wilbert <joao.wilbert@wk.com>
2011-06-07 22:28:33 +0100

    introduced fade and bugfixed

--- javascript/albums.js
+++ javascript/albums.js
@@ -1,18 +1,28 @@
+window.onresize=function(){
+	console.debug();
+}
+
 /***************************************************** Declararations ***************************************/
+// Settings
+var feedUrl = 'http://localhost:8080/json'; // path to JSON service
 
-// Variables
-var feedUrl = 'http://localhost:8080/json';
+var bufferLimit = 100; // how many images are loaded from JSON
+var initialSize = 316; // size (w x h) of each cover
+var frameRate = 1000/30;
 
-var buffer = [];
-var bufferLimit = 30;
-var initialSize = 300;
+// General Variables
 var l = 0;
+var buffer = [];
 var totalColumns;
 
 // Swap Timer Variables
+var t;
 var c = 0;
 var timer_is_on = 0;
-var t;
+
+// States
+var position = 0; // position 0=stack and 1=tile
+
 
 
 /***************************************************** Jquery Init Stuff ***************************************/
@@ -38,7 +48,17 @@ $(document).ready(function(){
 		}
 		
 		displayCovers(buffer);
-		positionAsStack(initialSize);
+		
+		
+		if(position == 0) {
+			// stack
+			positionAsStack(initialSize);
+		} else {
+			positionAsTile(initialSize);
+		}
+		
+		
+		
 			
 	});
 	
@@ -47,30 +67,39 @@ $(document).ready(function(){
 /***************************************************** Tweening Functions ***************************************/
 
 function switchToStack() {
+	position = 0;
 	positionAsStack(initialSize);
-	
 }
 
 function switchToTile() {
+	/*
+	t1 = new Tween(document.getElementById('14').style,'left',Tween.regularEaseOut,0,-1,2,'px');
+	t2 = new Tween(document.getElementById('14').style,'top',Tween.regularEaseOut,0,-1,2,'px');
+	t1.start();
+	t2.start();
+	*/
+	position = 1;
 	positionAsTile(initialSize, buffer)
 }
+
+
+
 
 /***************************************************** Position Functions ***************************************/
 
 // One cover Image Swaps
 function positionAsStack(initialSize) {
 	$.each(buffer, function(i, l){
-	
-		$("#"+i).css("left","0");
-		$("#"+i).css("top","0");
+		var posX = getResolution()[0] / 2 - initialSize/2;
+		var posY = getResolution()[1] / 2 - initialSize/2;
+		$("#"+i).css("left",posX);
+		$("#"+i).css("top",posY);
+
 	});
 
-	var posX = getResolution()[0] / 2 - initialSize/2;
-	var posY = getResolution()[1] / 2 - initialSize/2;
-	$("#coverStack").css("left",posX);
-	$("#coverStack").css("top",posY);	
 
 }
+
 
 function positionAsTile(initialSize,buffer) {
 	
@@ -89,18 +118,61 @@ function positionAsTile(initialSize,buffer) {
 	
 		$("#"+i).css("left",tilePosX);
 		$("#"+i).css("top",tilePosY);
+		
+
 	});
 
 }
 
+function updatePosition() {
+	
+}
+
+//window.onresize = positionAsTile(); 
 
 /***************************************************** Swap Functions **************************************/
 
 function displayCovers(buffer) {
+	$("#coverStack").css("display", "none");
+	
 	$.each(buffer, function(i, l){
 	   $(document.createElement("img")).attr({ src: l }).addClass("stack").appendTo("#coverStack").css("width",initialSize).css("height",initialSize).css("z-index",'1').attr("id",i);
+		
 	});
+	
+	if(allImagesLoaded() == 1) {
+		console.debug("test");
+		$("#coverStack").css("display", "block");
+		
+		$('#coverStack').fadeIn('slow', function() {
+	     // alert("Animation complete");
+	    });
+	
+	}
+
+
 }
+
+function allImagesLoaded() {
+
+	// return variable
+	var imagesloaded = 1;
+
+	// All images are saved in an array called document.images. Very usefull
+	var img = document.images;
+	// Loop through all the images
+	for (var i = 0;i<img.length;i++)
+	{
+		// If the image isnt loaded we set the return varible to 0
+		if(img[i].complete == false) {
+			imagesloaded = 0;
+		}
+	}
+
+	// This will return 0 if one or more images are not loaded and 1 if all images are loaded.
+	return imagesloaded;
+}
+
 
 function changeIndex() {	
 	l = l+1;
@@ -133,11 +205,11 @@ function handleOut(id) {
 // Swap Timmer
 function timedCount() {
 	c=c+1;
-	t = setTimeout("timedCount()",1000/30);
+	t = setTimeout("timedCount()",frameRate);
 	changeIndex();
 }
 
-// Helpers
+// Window Events
 function getResolution() {
 	 var viewportwidth;
 	 var viewportheight;
@@ -162,7 +234,12 @@ function getResolution() {
 	resolution[1] = viewportheight;
 	
 	// array - [0] width [1] height
+	console.debug()
+	
 	return resolution;
+
 }
 
-
+window.onresize=function(){
+	updatePosition();
+}
